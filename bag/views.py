8ee1eba2a81a3b9d9bbe30be6fad40903792a98b
Vladimir Bundalo <bundalo.vladimir@gmail.com>
2021-01-25 22:27:14 +0000

    Adding Bag Content functionality

--- bag/views.py
+++ bag/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render
 def view_bag(request):
     """ A view to renders the bag contents page """
 
-    return render(request, 'home/index.html')
+    return render(request, 'bag/bag.html')
