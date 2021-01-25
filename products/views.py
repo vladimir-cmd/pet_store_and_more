c3fb19643e4d05856801a9639d237808bd8a99e5
Vladimir Bundalo <bundalo.vladimir@gmail.com>
2021-01-25 21:27:40 +0000

    Adding Dropdown sort by functionality and sort section in views.py

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.db.models.functions import Lower
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from .models import Product, Category
@@ -22,7 +23,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
