8254f20c814770ed9e4edfcc809217d66079e177
Vladimir Bundalo <bundalo.vladimir@gmail.com>
2021-01-31 16:15:08 +0000

    Added Checkout Form and Template View with Order Summary functionality

--- checkout/forms.py
+++ checkout/forms.py
@@ -28,7 +28,7 @@ class OrderForm(forms.ModelForm):
             'county': 'County',
         }
 
-        self.fields['full_name'].widgets.attrs['autofocus'] = True
+        self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if self.fields[field].required:
                 placeholder = f'{placeholders[field]} *'
--- pet_store_and_more/settings.py
+++ pet_store_and_more/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'pet_store_and_more.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -62,8 +65,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
