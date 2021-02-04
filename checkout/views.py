376aeb2c9f2fc7b03c9a8d0d3afc616702a52572
Vladimir Bundalo <vbundalo@munichre.digital>
2021-02-04 21:26:52 +0000

    Initial Stripe JS and CSS added to Checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IHDzPLhMGhoweMgmkDmCxFwpuxmLma5PpmbnIIVMAa7nqZjIe72FYGKQkkofPjy91h17SbQO7D1NvocIUCNdThk00qIURxA0N',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
