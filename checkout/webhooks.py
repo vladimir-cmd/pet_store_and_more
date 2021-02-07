8e3ad25b298ac58a44622bee03f0c37c1a1afcc0
Vladimir Bundalo <bundalo.vladimir@gmail.com>
2021-02-07 16:33:14 +0000

    Improving webhook handler payment succeeded

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -30,7 +30,7 @@ def webhook(request):
     except Exception as e:
         return HttpResponse(content=e, status=400)
 
-    handler = StripeWH_Handler(request)
+    handler = StripeWH_handler(request)
 
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
