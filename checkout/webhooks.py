074a5d875c304478701b61f1ec1e18e5b9f4520a
Vladimir Bundalo <bundalo.vladimir@gmail.com>
2021-02-07 14:38:39 +0000

    Tested Stripe webhook locally with Stripe CLI

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -30,5 +30,16 @@ def webhook(request):
     except Exception as e:
         return HttpResponse(content=e, status=400)
 
-    print('Success!')
-    return HttpResponse(status=200)
+    handler = StripeWH_Handler(request)
+
+    event_map = {
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.payment_failed': handler.handle_payment_intent_failed,
+    }
+
+    event_type = event['type']
+
+    event_handler = event_map.get(event_type, handler.handle_event)
+
+    response = event_handler(event)
+    return response
