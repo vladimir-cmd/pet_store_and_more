cf0003bb5b9fd1736855c40ab8f1afcbedd06d77
sandravlado <vladimir.bundalo@outlook.com>
2021-02-09 19:34:10 +0000

    Adding Django Countries for a Country Dropdown and Selct in Checkout

--- checkout/forms.py
+++ checkout/forms.py
@@ -24,16 +24,16 @@ class OrderForm(forms.ModelForm):
             'street_address2': 'Street Address 2',
             'town_or_city': 'Town Or City',
             'postcode': 'Postal Code',
-            'country': 'Country',
             'county': 'County',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
--- pet_store_and_more/settings.py
+++ pet_store_and_more/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'checkout',
     'crispy_forms',
     'storages',
+    'profiles',
 ]
 
 MIDDLEWARE = [
