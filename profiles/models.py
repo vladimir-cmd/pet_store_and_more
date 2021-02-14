33aaca3138357e992a6a787ceec79db1271af628
sandravlado <vladimir.bundalo@outlook.com>
2021-02-14 14:13:26 +0000

    Created profiles template - configured urls and views files - migrated files

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 from django_countries.fields import CountryField
+
 
 # Create your models here.
 class UserProfile(models.Model):
@@ -15,3 +18,11 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
