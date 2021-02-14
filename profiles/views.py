03f5c35f01547cdcda4895a9e3172dcead390287
sandravlado <vladimir.bundalo@outlook.com>
2021-02-14 16:19:16 +0000

    working on a profile form

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 # Create your views here.
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
     return render(request, template, context)
