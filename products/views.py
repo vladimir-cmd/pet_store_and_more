2c5fe5e9a2b666a197e354bb2f83998057e5245a
sandravlado <vladimir.bundalo@outlook.com>
2021-02-21 18:04:52 +0000

    Finished with Product Management functionality

--- products/views.py
+++ products/views.py
@@ -128,7 +128,7 @@ def delete_product(request, product_id):
     if not request.user.is_superuser:
         messages.error(request, "You don't have enough permissions to add product. Please contact administrator.")
         return redirect(reverse('home'))
-        
+
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product deleted!')
