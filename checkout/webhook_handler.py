d3f09dfb7fd6b28f240e61b5d417031f2d1a6199
Vladimir Bundalo <bundalo.vladimir@gmail.com>
2021-02-23 11:15:34 +0000

    Troubleshooting webhook handler - Checking the Payment Succeed workflow

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -52,6 +52,7 @@ class StripeWH_Handler:
         print("Step 2")
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
+        print("Shipping Details: {}".format(shipping_details))
         grand_total = round(intent.charges.data[0].amount / 100, 2)
         print("Step 3")
         for field, value in shipping_details.address.items():
@@ -83,7 +84,7 @@ class StripeWH_Handler:
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
                     country__iexact=shipping_details.address.country,
-                    postcode__iexact=shipping_details.address.postcode,
+                    postcode__iexact=shipping_details.address.postal_code,
                     town_or_city__iexact=shipping_details.address.city,
                     street_address1__iexact=shipping_details.address.line1,
                     street_address2__iexact=shipping_details.address.line2,
@@ -95,8 +96,10 @@ class StripeWH_Handler:
                 order_exists = True
                 break
             except Order.DoesNotExist:
+                print("We are in order does not exists - attempt: {}".format(attempt))
                 attempt += 1
                 time.sleep(1)
+                
         print("Step 6")
         if order_exists:
             self._send_confirmation_email(order)
@@ -113,7 +116,7 @@ class StripeWH_Handler:
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
-                    postcode=shipping_details.address.postcode,
+                    postcode=shipping_details.address.postal_code,
                     town_or_city=shipping_details.address.city,
                     street_address1=shipping_details.address.line1,
                     street_address2=shipping_details.address.line2,
