64b2346d9b6f93729353ffef101adb939bf4786a
Vladimir Bundalo <bundalo.vladimir@gmail.com>
2021-02-24 11:07:52 +0000

    Removing whole order selection from if order exists loop

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -82,20 +82,6 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(5)
         if order_exists:
-            order = Order.objects.get(
-                    full_name__iexact=shipping_details.name,
-                    email__iexact=billing_details.email,
-                    phone_number__iexact=shipping_details.phone,
-                    country__iexact=shipping_details.address.country,
-                    postcode__iexact=shipping_details.address.postal_code,
-                    town_or_city__iexact=shipping_details.address.city,
-                    street_address1__iexact=shipping_details.address.line1,
-                    street_address2__iexact=shipping_details.address.line2,
-                    county__iexact=shipping_details.address.state,
-                    grand_total=grand_total,
-                    original_bag=bag,
-                    stripe_pid=pid
-                )
             self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCEESS: verified order already in database',
