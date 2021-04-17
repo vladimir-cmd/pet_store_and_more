112348d7f13fc53dea99e462ac6b402d1eb86f71
Vladimir Bundalo <bundalo.vladimir@gmail.com>
2021-04-17 23:08:48 +0100

    Added Front Page Link to Birds section and Cats section

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('specific_products_dog/', views.specific_products_dog, name='specific_products_dog'),
     path('specific_products_cat/', views.specific_products_cat, name='specific_products_cat'),
+    path('specific_products_bird/', views.specific_products_bird, name='specific_products_bird'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
