5d8c0a35d3d6596c254eb492d407a741c9050581
Vladimir Bundalo <bundalo.vladimir@gmail.com>
2021-04-22 16:00:04 +0100

    Corrected email host dns

--- pet_store_and_more/settings.py
+++ pet_store_and_more/settings.py
@@ -12,8 +12,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = 'TRUE'
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'TRUE'
 
 ALLOWED_HOSTS = [
     'pet-store-and-more.herokuapp.com',
@@ -186,10 +186,10 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if "DEVELOPMENT" in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_SHOST = 'smtp.google.com'
+    EMAIL_HOST = 'smtp.google.com'
     EMAIL_HOST_USER = os.environ.get('bundalo.vladimir@gmail.com')
     EMAIL_HOST_PASSWORD = os.environ.get('qyysntwsbbvdhxpd')
     DEFAULT_FROM_EMAIL = os.environ.get('bundalo.vladimir@gmail.com')
@@ -198,7 +198,7 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_SHOST = 'smtp.google.com'
+    EMAIL_HOST = 'smtp.google.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
