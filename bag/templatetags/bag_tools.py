99e2a10c72ba01c8ef3e95eb6512699b4b1818e4
Vladimir Bundalo <bundalo.vladimir@gmail.com>
2021-04-22 10:41:09 +0100

    Bag details and info updated to properly display the price per size and calculate subtotal

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -1,8 +1,11 @@
 from django import template
+import decimal
 
 
 register = template.Library()
 
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
-    return price * quantity
+    price = price.strip()
+    if price:
+        return decimal.Decimal(price) * quantity
