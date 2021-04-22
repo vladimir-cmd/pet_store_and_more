96e4299f3d33c181aaeb90352baa2531e57bdd9c
Vladimir Bundalo <bundalo.vladimir@gmail.com>
2021-04-22 17:07:54 +0100

    Email registration info has been fixed. Wrong SMTP server provided

--- pet_store_and_more/settings.py
+++ pet_store_and_more/settings.py
@@ -187,22 +187,13 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if "DEVELOPMENT" in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.google.com'
-    EMAIL_HOST_USER = 'bundalo.vladimir@gmail.com'
-    EMAIL_HOST_PASSWORD = 'F32@t556*(!@#'
-    DEFAULT_FROM_EMAIL = 'bundalo.vladimir@gmail.com'
-    # EMAIL_HOST_USER = os.environ.get('bundalo.vladimir@gmail.com')
-    # EMAIL_HOST_PASSWORD = os.environ.get('qyysntwsbbvdhxpd')
-    # DEFAULT_FROM_EMAIL = os.environ.get('bundalo.vladimir@gmail.com')
-    # DEFAULT_FROM_EMAIL = 'petstoreadmin@petstoreandmore.com'
+    DEFAULT_FROM_EMAIL = 'petstoreadmin@petstoreandmore.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.google.com'
-    EMAIL_HOST_USER = 'bundalo.vladimir@gmail.com'
-    EMAIL_HOST_PASSWORD = 'F32@t556*(!@#'
-    DEFAULT_FROM_EMAIL = 'bundalo.vladimir@gmail.com'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
     
