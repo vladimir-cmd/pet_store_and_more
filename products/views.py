49159d8112c94bf8766619526ba6d0592d3a8799
Vladimir Bundalo <bundalo.vladimir@gmail.com>
2021-04-22 14:11:43 +0100

    Implemented sorting per category and deeper selection of specific category within major category - Fixed URL callbacks for specific category in HTML

--- products/views.py
+++ products/views.py
@@ -7,121 +7,248 @@ from .models import Product, Category
 from .forms import ProductForm
 
 # Create your views here.
+
+
 def specific_products_dog(request):
-    print('Starting with the function')
-    
-    try:
-        categories = Category.objects.all()
-        products = Product.objects.all()
-        print('Get the products: {}'.format(products))
-        print('Get the categories: {}'.format(categories))
-    except Exception as e:
-        print('There was an issue with bloody category: {}'.format(str(e)))
-    
-    print('We are in get request')
-    try:
-        # if 'dry_dog_food' in categories:
-        print('We are in dog check')
-        categories = Category.objects.filter(name__contains='dog')
-        print(categories)
-        products = products.filter(category__name__contains='dog')
-        print(products)
-    except Exception as e:
-        print('There was an issue with enumrating: {}'.format(str(e)))
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    categories = Category.objects.filter(name__contains='dog')
+    products = products.filter(category__name__contains='dog')
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if sortkey == 'price':
+                if direction == 'asc':
+                    sortkey = 's_price'
+                else:
+                    sortkey = 'x_price'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
+        'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
+
     return render(request, 'products/specific_products_dog.html', context)
-   
+
 
 def specific_products_cat(request):
-    print('Starting with the function')
-    
-    try:
-        categories = Category.objects.all()
-        products = Product.objects.all()
-        print('Get the products: {}'.format(products))
-        print('Get the categories: {}'.format(categories))
-    except Exception as e:
-        print('There was an issue with bloody category: {}'.format(str(e)))
-    
-    print('We are in get request')
-    try:
-        # if 'dry_dog_food' in categories:
-        print('We are in cat check')
-        categories = Category.objects.filter(name__contains='cat')
-        print(categories)
-        products = products.filter(category__name__contains='cat')
-        print(products)
-    except Exception as e:
-        print('There was an issue with enumrating: {}'.format(str(e)))
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    categories = Category.objects.filter(name__contains='cat')
+    products = products.filter(category__name__contains='cat')
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if sortkey == 'price':
+                if direction == 'asc':
+                    sortkey = 's_price'
+                else:
+                    sortkey = 'x_price'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
+        'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
+
     return render(request, 'products/specific_products_cat.html', context)
-   
+
 
 def specific_products_bird(request):
-    print('Starting with the function')
-    
-    try:
-        categories = Category.objects.all()
-        products = Product.objects.all()
-        print('Get the products: {}'.format(products))
-        print('Get the categories: {}'.format(categories))
-    except Exception as e:
-        print('There was an issue with bloody category: {}'.format(str(e)))
-    
-    print('We are in get request')
-    try:
-        # if 'dry_dog_food' in categories:
-        print('We are in bird check')
-        categories = Category.objects.filter(name__contains='bird')
-        print(categories)
-        products = products.filter(category__name__contains='bird')
-        print(products)
-    except Exception as e:
-        print('There was an issue with enumrating: {}'.format(str(e)))
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    categories = Category.objects.filter(name__contains='bird')
+    products = products.filter(category__name__contains='bird')
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if sortkey == 'price':
+                if direction == 'asc':
+                    sortkey = 's_price'
+                else:
+                    sortkey = 'x_price'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
+        'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
+
     return render(request, 'products/specific_products_bird.html', context)
 
 
 def specific_products_dealz(request):
-    print('Starting with the function')
-    
-    try:
-        categories = Category.objects.all()
-        products = Product.objects.all()
-        print('Get the products: {}'.format(products))
-        print('Get the categories: {}'.format(categories))
-    except Exception as e:
-        print('There was an issue with bloody category: {}'.format(str(e)))
-    
-    print('We are in get request')
-    try:
-        # if 'dry_dog_food' in categories:
-        print('We are in bird check')
-        categories = Category.objects.filter(name__in=['deals','clearance','new_items'])
-        print(categories)
-        products = products.filter(category__name__in=['deals','clearance','new_items'])
-        print(products)
-    except Exception as e:
-        print('There was an issue with enumrating: {}'.format(str(e)))
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    categories = Category.objects.filter(
+        name__in=['deals', 'clearance', 'new_items'])
+    products = products.filter(
+        category__name__in=['deals', 'clearance', 'new_items'])
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if sortkey == 'price':
+                if direction == 'asc':
+                    sortkey = 's_price'
+                else:
+                    sortkey = 'x_price'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
+        'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
+
     return render(request, 'products/specific_products_dealz.html', context)
-   
+
 
 def all_products(request):
     """ A view to return the index page """
@@ -160,10 +287,12 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria!")
+                messages.error(
+                    request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
@@ -191,7 +320,8 @@ def product_detail(request, product_id):
 @login_required
 def add_product(request):
     if not request.user.is_superuser:
-        messages.error(request, "You don't have enough permissions to add product. Please contact administrator.")
+        messages.error(
+            request, "You don't have enough permissions to add product. Please contact administrator.")
         return redirect(reverse('home'))
 
     if request.method == 'POST':
@@ -201,7 +331,8 @@ def add_product(request):
             messages.success(request, 'Successfully added product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(
+                request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm()
 
@@ -216,7 +347,8 @@ def add_product(request):
 @login_required
 def edit_product(request, product_id):
     if not request.user.is_superuser:
-        messages.error(request, "You don't have enough permissions to add product. Please contact administrator.")
+        messages.error(
+            request, "You don't have enough permissions to add product. Please contact administrator.")
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
@@ -228,7 +360,8 @@ def edit_product(request, product_id):
             messages.success(request, 'Successfully updated product!')
             return redirect(reverse('product_detail', args=[product_id]))
         else:
-            messages.error(request, 'Failed to update product. Please ensure that form is valid.')
+            messages.error(
+                request, 'Failed to update product. Please ensure that form is valid.')
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
@@ -245,7 +378,8 @@ def edit_product(request, product_id):
 @login_required
 def delete_product(request, product_id):
     if not request.user.is_superuser:
-        messages.error(request, "You don't have enough permissions to add product. Please contact administrator.")
+        messages.error(
+            request, "You don't have enough permissions to add product. Please contact administrator.")
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
